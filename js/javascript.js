53007f9926d89fe353a0a3a296f9d78cd5b026a1
Jorge Ernesto Chavez Puente <jrgchvzpnt@gmail.com>
2023-11-13 17:45:58 -0700

    Se ordena codigo

--- js/javascript.js
+++ js/javascript.js
@@ -1,51 +1,59 @@
-function crearheader(doc,direccionX,inicio,abajo) {
-         // ************************ INCIO DEL HEADER *********************************
+function crearheader(doc, direccionX, inicio, abajo) {
+    // ************************ INCIO DEL HEADER *********************************
     doc.setFontSize(9);
     doc.setFont("Arial", "bold");
+
+    // Fecha
     doc.text("Fecha:", inicio, abajo);
     doc.setFont("Arial", "");
-    doc.text("MARTES, 22 DE OCTUBRE DE 2023", direccionX, abajo  );
+    doc.text("MARTES, 22 DE OCTUBRE DE 2023", direccionX, abajo);
 
+    // Paciente
     doc.setFont("Arial", "bold");
-    doc.text("Paciente:", inicio, abajo+6);
+    doc.text("Paciente:", inicio, abajo + 6);
     doc.setFont("Arial", "");
-    doc.text("JORGE ERNESTO CHAVEZ PUENTE", direccionX, abajo+6 );
-   
-    doc.setFont("Arial", "bold");
-    doc.text("Edad:", inicio, abajo+12);
-    doc.setFont("Arial", "");
-    doc.text("70 AÑOS", direccionX, abajo+12);
+    doc.text("JORGE ERNESTO CHAVEZ PUENTE", direccionX, abajo + 6);
 
+    // Edad
     doc.setFont("Arial", "bold");
-    doc.text("Fecha de Nacimiento: ", 105, abajo+12);
+    doc.text("Edad:", inicio, abajo + 12);
     doc.setFont("Arial", "");
-    doc.text("08/OCT/1985", 150,  abajo+12);
+    doc.text("70 AÑOS", direccionX, abajo + 12);
 
+    // Fecha de Nacimiento
     doc.setFont("Arial", "bold");
-    doc.text("Sexo:", inicio, abajo+18);
+    doc.text("Fecha de Nacimiento: ", 105, abajo + 12);
     doc.setFont("Arial", "");
-    doc.text("MASCULINO", direccionX, abajo+18);
+    doc.text("08/OCT/1985", 150, abajo + 12);
 
+    // Sexo
     doc.setFont("Arial", "bold");
-    doc.text("Médico solicitante:", 105, abajo+18);
+    doc.text("Sexo:", inicio, abajo + 18);
     doc.setFont("Arial", "");
-    doc.text("AQC", 150, abajo+18);
+    doc.text("MASCULINO", direccionX, abajo + 18);
 
+    // Médico solicitante
     doc.setFont("Arial", "bold");
-    doc.text("Sucursal: ", inicio, abajo+24);
+    doc.text("Médico solicitante:", 105, abajo + 18);
     doc.setFont("Arial", "");
-    doc.text("CULIACAN", direccionX, abajo+24);
+    doc.text("AQC", 150, abajo + 18);
 
+    // Sucursal
     doc.setFont("Arial", "bold");
-    doc.text("Nombre del estudio: ", inicio, abajo+30);
+    doc.text("Sucursal: ", inicio, abajo + 24);
     doc.setFont("Arial", "");
-    doc.text("ULTRASONIDO HIGADO Y VIAS BILIARES ", direccionX, abajo+30);
+    doc.text("CULIACAN", direccionX, abajo + 24);
+
+    // Nombre del estudio
+    doc.setFont("Arial", "bold");
+    doc.text("Nombre del estudio: ", inicio, abajo + 30);
+    doc.setFont("Arial", "");
+    doc.text("ULTRASONIDO HIGADO Y VIAS BILIARES ", direccionX, abajo + 30);
 
     // ************************ CIERRE DEL HEADER *********************************
-  }
+}
 
-
-  function crearbody(doc,inicio,abajo){
+function crearbody(doc,inicio,abajo){
                     
     doc.setFont("Arial", "bold");
     doc.text("Indicación del estudio: ___ ", inicio, abajo+45);
@@ -124,8 +132,6 @@ function crearheader(doc,direccionX,inicio,abajo) {
     doc.setFont("Arial", "bold");
     doc.text("Páncreas: ", inicio, abajo + 105);
  
-
-
     doc.setFont("Arial", "");
     textoLargo = "Con forma, tamaño y contornos normales, patrón ecográfico homogéneo. No observo colecciones en el espacio peripancreático.";
     lines = doc.splitTextToSize(textoLargo, maxWidth);
@@ -172,71 +178,64 @@ function crearheader(doc,direccionX,inicio,abajo) {
      lines.forEach((line, index) => {
         doc.text(line, inicio + 24, abajo + 200 + index * lineHeight);
     });
+}
+function generartable(doc)  {
+    const headers = ["Dimensiones", "Diámetro longitudinal", "Diámetro anteroposterior", "Diámetro transversal", "Espesor cortical"];
+
+    const data = [
+        ["Riñón derecho", "___mm", "___mm", "___mm", "___mm"],
+        ["Riñón izquierdo", "___mm", "___mm", "___mm", "___mm"]
+    ];
+
+    // Configuración de la tabla con diferentes colores de fondo para la cabecera y el cuerpo
+    const tableConfig = {
+        startY: 165,
+        startX: 5,
+        head: [headers],
+        body: data,
+        styles: {
+            head: { fillColor: [128, 128, 128], textColor: [0, 0, 0], align: "center" }, // Cabecera con fondo gris
+        }
+    };
+
+    doc.autoTable(tableConfig);
+}
+
+function generarbarcode(doc){
+
+    // Adding a barcode
+    JsBarcode("#barcode", "*RSV729551072*", {
+        format: "CODE128",
+        displayValue: true
+    });
+
+    // Adding barcode to PDF
+    const barcodeSVG = document.querySelector('#barcode');
+    const svgData = barcodeSVG.outerHTML;
+
+    const imgBarcode = new Image();
+    imgBarcode.src = 'data:image/svg+xml;base64,' + btoa(unescape(encodeURIComponent(svgData)));
+
+    // Obtiene la posición X y Y para la adición del código de barras
+    const barcodeX = 105; // Ajusta según sea necesario
+    const barcodeY = 15;  // Ajusta según sea necesario
+
+    imgBarcode.onload = function() {
+        const canvasBarcode = document.createElement('canvas');
+        canvasBarcode.width = imgBarcode.width;
+        canvasBarcode.height = imgBarcode.height;
+        const contextBarcode = canvasBarcode.getContext('2d');
+        contextBarcode.drawImage(imgBarcode, 0, 0);
+        const imageDataBarcode = canvasBarcode.toDataURL('image/png');
+        doc.addImage(imageDataBarcode, 'PNG', barcodeX, barcodeY, 50, 15);
+    };
+}
+
+function generarfooter(doc){
+    const pageCount = doc.internal.getNumberOfPages();
+    doc.setFontSize(10);
+    for (let i = 1; i <= pageCount; i++) {
+        doc.setPage(i);
+        doc.text(`Pagína ${i} of ${pageCount}`, 150, 285);
     }
-
-    function generartable(doc) 
-            {
-                const headers = ["Dimensiones", "Diámetro longitudinal", "Diámetro anteroposterior", "Diámetro transversal", "Espesor cortical"];
-
-                const data = [
-                    ["Riñón derecho", "___mm", "___mm", "___mm", "___mm"],
-                    ["Riñón izquierdo", "___mm", "___mm", "___mm", "___mm"]
-                ];
-
-                // Configuración de la tabla con diferentes colores de fondo para la cabecera y el cuerpo
-                const tableConfig = {
-                    startY: 165,
-                    startX: 5,
-                    head: [headers],
-                    body: data,
-                    styles: {
-                        head: { fillColor: [128, 128, 128], textColor: [0, 0, 0], align: "center" }, // Cabecera con fondo gris
-                    }
-                };
-
-                doc.autoTable(tableConfig);
-            }
-
-
-            
-            function generarbarcode(doc){
-
-                // Adding a barcode
-                JsBarcode("#barcode", "*RSV729551072*", {
-                    format: "CODE128",
-                    displayValue: true
-                });
-
-                // Adding barcode to PDF
-                const barcodeSVG = document.querySelector('#barcode');
-                const svgData = barcodeSVG.outerHTML;
-
-                const imgBarcode = new Image();
-                imgBarcode.src = 'data:image/svg+xml;base64,' + btoa(unescape(encodeURIComponent(svgData)));
-
-                // Obtiene la posición X y Y para la adición del código de barras
-                const barcodeX = 105; // Ajusta según sea necesario
-                const barcodeY = 15;  // Ajusta según sea necesario
-
-                imgBarcode.onload = function() {
-                    const canvasBarcode = document.createElement('canvas');
-                    canvasBarcode.width = imgBarcode.width;
-                    canvasBarcode.height = imgBarcode.height;
-                    const contextBarcode = canvasBarcode.getContext('2d');
-                    contextBarcode.drawImage(imgBarcode, 0, 0);
-                    const imageDataBarcode = canvasBarcode.toDataURL('image/png');
-                    doc.addImage(imageDataBarcode, 'PNG', barcodeX, barcodeY, 50, 15);
-
-                    // Save PDF
-                  
-                };
-            }
-
-            function generarfooter(doc){
-                const pageCount = doc.internal.getNumberOfPages();
-                doc.setFontSize(10);
-                for (let i = 1; i <= pageCount; i++) {
-                    doc.setPage(i);
-                    doc.text(`Pagína ${i} of ${pageCount}`, 150, 285);
-                }
-            }
+}
